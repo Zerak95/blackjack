e2acf502846d2b8a4c9bc86d88571ffa391e47b1
Zaid Soboh <zaidsoboh@Zaids-MacBook-Pro.local>
2022-10-09 21:20:14 +1100

    pseudocode for basic gmae

---
+++ blackjack-game.js
@@ -0,0 +1,13 @@
+console.log('JS connected');
+
+//create a deck of cards
+
+//create player and dealer objects
+    //name, hand, wins
+    
+
+//create random function to hand out cards
+
+//caluclate if the player wins
+
+//end gmae or draw a card
